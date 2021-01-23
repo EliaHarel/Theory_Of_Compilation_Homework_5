2d321734dba75a0342e6925e0c7f26f69ce07419
EliaHarel <EliaHarel@users.noreply.github.com>
2021-01-23 16:00:00 +0200

    Update expression.h

--- expression.h
+++ expression.h
@@ -12,53 +12,60 @@
 #include "bp.hpp"
 
 
-extern int yylineno;
+typedef std::string Label;
 
 class Expression {
+    vector<pair<int, BranchLabelIndex>> true_list = vector<pair<int, BranchLabelIndex>>();
+    vector<pair<int, BranchLabelIndex>> false_list = vector<pair<int, BranchLabelIndex>>();
     static int new_var_counter;
 
-    bool isNum() const {
-        return type == Types_enum::INT_TYPE || type == Types_enum::BYTE_TYPE;
-    }
+    bool isNum() const{ return type == Types_enum::INT_TYPE || type == Types_enum::BYTE_TYPE; }
 
 public:
     Types type;
     std::string var_name;
 
-    Expression() : type(Types_enum::UNDEFINED_TYPE), var_name(gimmeANewCuteVar()) {};
+    Expression() : type(Types_enum::UNDEFINED_TYPE), var_name(gimmeANewCuteVar()){};
 
-    explicit Expression(const Types &t) : type(t), var_name(gimmeANewCuteVar()) {};
+    explicit Expression(const Types& t) : type(t), var_name(gimmeANewCuteVar()){};
 
-    explicit Expression(Types_enum t) : type(t), var_name(gimmeANewCuteVar()) {};
+    explicit Expression(Types_enum t) : type(t), var_name(gimmeANewCuteVar()){};
 
-    Expression(Types_enum t, const string& str) : type(t), var_name(str) {};
+    Expression(Types_enum t, const std::string& str) : type(t), var_name(str){};
 
-    Expression(const Types& t, const string& str) : type(t), var_name(str) {};
+    Expression(const Types& t, const std::string& str) : type(t), var_name(str){};
 
 
     ~Expression() = default;
 
-    Expression(const Expression &rhs) : type(rhs.type), var_name(rhs.var_name) {};
+    Expression(const Expression& rhs) : type(rhs.type), var_name(rhs.var_name),
+                                        true_list(rhs.true_list), false_list(rhs.false_list){};
 
-    Expression &operator=(const Expression &rhs) {
-        if (this == &rhs) { return *this; }
+    Expression& operator=(const Expression& rhs){
+        if(this == &rhs){ return *this; }
         type = rhs.type;
+        true_list = rhs.true_list;
+        false_list = rhs.false_list;
         var_name = gimmeANewCuteVar();
         return *this;
     }
 
-    Expression opNot() {
-        if (type != Types_enum::BOOL_TYPE) {
+    Expression opNot(){
+        if(type != Types_enum::BOOL_TYPE){
             output::errorMismatch(yylineno);
             exit(1);
         }
-        Expression new_var(Types_enum::BOOL_TYPE);
-        CodeBuffer::instance().emit(new_var.var_name + " = sub i1 1, " + var_name);
+//        Expression new_var(Types_enum::BOOL_TYPE);
+//        CodeBuffer::instance().emit(new_var.var_name + " = sub i1 1, " + var_name);
+        Expression new_var(*this);
+        vector<pair<int, BranchLabelIndex>> temp = new_var.false_list;
+        new_var.false_list = new_var.true_list;
+        new_var.true_list = temp;
         return new_var;
     }
 
-    Expression opOr(const Expression &t) {
-        if (type != Types_enum::BOOL_TYPE || t.type != Types_enum::BOOL_TYPE) {
+    Expression opOr(const Expression& t){
+        if(type != Types_enum::BOOL_TYPE || t.type != Types_enum::BOOL_TYPE){
             output::errorMismatch(yylineno);
             exit(1);
         }
@@ -80,7 +87,8 @@ public:
         string exit = CodeBuffer::instance().genLabel();
         Expression new_var(Types_enum::BOOL_TYPE);
         string phi_comm =
-                new_var.var_name + " = phi i1 [ " + val1 + ", " + false2_label + " ], [ " + val2 + ", " + true_label +
+                new_var.var_name + " = phi i1 [ " + val1 + ", " + false2_label + " ], [ " + val2 + ", " +
+                true_label +
                 " ]";
         CodeBuffer::instance().emit(phi_comm);
 
@@ -94,8 +102,8 @@ public:
         return new_var;
     }
 
-    Expression opAnd(const Expression &t) {
-        if (type != Types_enum::BOOL_TYPE || t.type != Types_enum::BOOL_TYPE) {
+    Expression opAnd(const Expression& t){
+        if(type != Types_enum::BOOL_TYPE || t.type != Types_enum::BOOL_TYPE){
             output::errorMismatch(yylineno);
             exit(1);
         }
@@ -116,7 +124,8 @@ public:
         string exit = CodeBuffer::instance().genLabel();
         Expression new_var(Types_enum::BOOL_TYPE);
         string phi_comm =
-                new_var.var_name + " = phi i1 [ " + val1 + ", " + false_label + " ], [ " + val2 + ", " + true2_label +
+                new_var.var_name + " = phi i1 [ " + val1 + ", " + false_label + " ], [ " + val2 + ", " +
+                true2_label +
                 " ]";
         CodeBuffer::instance().emit(phi_comm);
 
@@ -130,8 +139,8 @@ public:
         return new_var;
     }
 
-    Expression relop(const Expression &t) {
-        if (!isNum() || !t.isNum()) {
+    Expression relop(const Expression& t){
+        if(!isNum() || !t.isNum()){
             output::errorMismatch(yylineno);
             exit(1);
         }
@@ -139,13 +148,13 @@ public:
         return *this;
     }
 
-    Expression binopMulDiv( Expression t, std::string &op) {
+    Expression binopMulDiv(const Expression t, std::string& op){
         auto temp = byteIntCheck(*this, t);
         Expression new_var(temp);
         std::string op_llvm;
-        if (op == "*") {
+        if(op == "*"){
             op_llvm = "mul";
-        } else {
+        }else{
             string cond = gimmeANewCuteVar();
             CodeBuffer::instance().emit(cond + " = icmp eq i32 " + t.var_name + ", 0");
             int loc = CodeBuffer::instance().emit("br i1 " + cond + ", label @, label @");
@@ -161,14 +170,14 @@ public:
             CodeBuffer::instance().bpatch(CodeBuffer::makelist({loc, SECOND}), false_label);
             CodeBuffer::instance().bpatch(CodeBuffer::makelist({true_to_exit, FIRST}), false_label);
 
-            if (new_var.type == Types_enum::INT_TYPE) { //op == "/"
+            if(new_var.type == Types_enum::INT_TYPE){ //op == "/"
                 op_llvm = "sdiv";
-            } else { // op == "/" && new_var.type == Types_enum::BYTE_TYPE
+            }else{ // op == "/" && new_var.type == Types_enum::BYTE_TYPE
                 op_llvm = "udiv";
             }
         }
         print3ACArithmetic(new_var.var_name, var_name, op_llvm, t.var_name);
-        if (temp == Types_enum::BYTE_TYPE) {
+        if(temp == Types_enum::BYTE_TYPE){
             new_var.var_name = byteOverflow(new_var.var_name);
         }
 
@@ -176,14 +185,14 @@ public:
     }
 
 
-    Expression binopAddSub(const Expression t, std::string &op) {
+    Expression binopAddSub(const Expression t, std::string& op){
         std::string op_llvm;
-        if (op == "+") { op_llvm = "add"; }
+        if(op == "+"){ op_llvm = "add"; }
         else op_llvm = "sub"; //op == "-"
 
 
-        if (t.type.getType() == Types_enum::SET_TYPE) {
-            if (!isNum()) {
+        if(t.type.getType() == Types_enum::SET_TYPE){
+            if(!isNum()){
                 output::errorMismatch(yylineno);
                 exit(1);
             }
@@ -193,8 +202,8 @@ public:
             handleSet(t, new_var, "copy");
             changeSet(new_var, *this, op);
             return new_var;
-        } else if (type.getType() == Types_enum::SET_TYPE) {
-            if (!t.isNum()) {
+        }else if(type.getType() == Types_enum::SET_TYPE){
+            if(!t.isNum()){
                 output::errorMismatch(yylineno);
                 exit(1);
             }
@@ -204,18 +213,18 @@ public:
             handleSet(*this, new_var, "copy");
             changeSet(new_var, t, op);
             return new_var;
-        } else {
+        }else{
             Types_enum temp = byteIntCheck(*this, t);
             Expression new_var(temp);
             print3ACArithmetic(new_var.var_name, var_name, op_llvm, t.var_name);
-            if (temp == Types_enum::BYTE_TYPE) {
+            if(temp == Types_enum::BYTE_TYPE){
                 new_var.var_name = byteOverflow(new_var.var_name);
             }
             return new_var;
         }
     }
 
-    static string handleSet(const Expression &src, const Expression &dst, const string &func) {
+    static string handleSet(const Expression& src, const Expression& dst, const string& func){
 
         string init = CodeBuffer::instance().genLabel();
         string sum;
@@ -237,19 +246,21 @@ public:
 
         string false_label = CodeBuffer::instance().genLabel();
         string src_var = gimmeANewCuteVar();
-        std::string src_comm = "getelementptr [256 * i1], [256 * i1]* " + src.var_name + " i1 0, i1 " + counter;
+        std::string src_comm =
+                "getelementptr [256 * i1], [256 * i1]* " + src.var_name + " i1 0, i1 " + counter;
         CodeBuffer::instance().emit(src_var + " = " + src_comm);
 
-        if (func == "copy") {
+        if(func == "copy"){
             string val = gimmeANewCuteVar();
             CodeBuffer::instance().emit(val + " = load i1, i1* " + src_var);
             string dst_var = gimmeANewCuteVar();
-            std::string dst_comm = "getelementptr [256 * i1], [256 * i1]* " + dst.var_name + " i1 0, i1 " + counter;
+            std::string dst_comm =
+                    "getelementptr [256 * i1], [256 * i1]* " + dst.var_name + " i1 0, i1 " + counter;
             CodeBuffer::instance().emit(dst_var + " = " + src_comm);
             CodeBuffer::instance().emit("store i1 " + val + ", i1* " + dst_var);
-        } else if (func == "init") {
+        }else if(func == "init"){
             CodeBuffer::instance().emit("store i1 0 , i1* " + src_var);
-        } else { // func == "sum"
+        }else{ // func == "sum"
             string temp_sum = gimmeANewCuteVar();
             CodeBuffer::instance().emit(temp_sum + " = load i32, i32* " + sum);
             string val_from_set = gimmeANewCuteVar();
@@ -257,7 +268,7 @@ public:
             string val_from_set_i32 = gimmeANewCuteVar();
             CodeBuffer::instance().emit(val_from_set_i32 + " = zext i1 " + val_from_set + " to i32");
             string add = gimmeANewCuteVar();
-            print3ACArithmetic(add,val_from_set_i32, "add", temp_sum);
+            print3ACArithmetic(add, val_from_set_i32, "add", temp_sum);
             CodeBuffer::instance().emit("store i32 " + add + ", i32* " + sum);
         }
 
@@ -279,7 +290,7 @@ public:
         CodeBuffer::instance().bpatch(CodeBuffer::makelist({to_loop, FIRST}), loop);
 
         string sum_val;
-        if(func=="sum"){
+        if(func == "sum"){
             sum_val = gimmeANewCuteVar();
             CodeBuffer::instance().emit(sum_val + " = load i32, i32* " + sum);
         }
@@ -287,8 +298,8 @@ public:
     }
 
 
-    Expression in(const Expression &t) {
-        if (!isNum() || t.type.getType() != Types_enum::SET_TYPE) {
+    Expression in(const Expression& t){
+        if(!isNum() || t.type.getType() != Types_enum::SET_TYPE){
             output::errorMismatch(yylineno);
             exit(1);
         }
@@ -316,7 +327,8 @@ public:
         string end_if_label = CodeBuffer::instance().genLabel();
         Expression new_var(Types_enum::BOOL_TYPE);
         string phi_comm =
-                new_var.var_name + " = phi i1 [ " + val1 + ", " + false_label + " ], [ " + val2 + ", " + true_label +
+                new_var.var_name + " = phi i1 [ " + val1 + ", " + false_label + " ], [ " + val2 + ", " +
+                true_label +
                 " ]";
         CodeBuffer::instance().emit(phi_comm);
 
@@ -329,29 +341,30 @@ public:
         return new_var;
     }
 
-    static void changeSet(const Expression &set, const Expression &num, const std::string &op) {
+    static void changeSet(const Expression& set, const Expression& num, const std::string& op){
         checkSetVal(set, num, op);
         std::string loc_in_mem = getLocInSet(set, num);
         int val = (op == "add") ? 1 : 0;
         CodeBuffer::instance().emit("store i1 " + to_string(val) + ", i1* " + loc_in_mem);
     }
 
-    static void checkSetVal(const Expression &set, const Expression &num, const std::string &op) {
+    static void checkSetVal(const Expression& set, const Expression& num, const std::string& op){
         string cond1_name = gimmeANewCuteVar();
         //sge: interprets the operands as signed values and yields true if op1 is greater than or equal to op2.
         string cond1_code =
-                cond1_name + " = icmp sge i32 " + num.var_name + ", " + to_string(set.type.getSetRange().first);
+                cond1_name + " = icmp sge i32 " + num.var_name + ", " +
+                to_string(set.type.getSetRange().first);
         CodeBuffer::instance().emit(cond1_code);
         int cond1_loc = CodeBuffer::instance().emit("br i1 " + cond1_name + ", label @, label @ ");
 
         string false_label = CodeBuffer::instance().genLabel();
         //todo: save thr string in advance and add reference to it
         //todo: calling to functions!
-        if (op == "add") {
+        if(op == "add"){
             CodeBuffer::instance().emit("call i8* @print( i8* Error out of set range. Op: +)");
-        } else if (op == "sub") {
+        }else if(op == "sub"){
             CodeBuffer::instance().emit("call i8* @print( i8* Error out of set range. Op: -)");
-        } else { // op == "in"
+        }else{ // op == "in"
             CodeBuffer::instance().emit("call i8* @print( i8* Error out of set range. Op: in)");
 
         }
@@ -363,7 +376,8 @@ public:
         string cond2_name = gimmeANewCuteVar();
         //sle: interprets the operands as signed values and yields true if op1 is less than or equal to op2..
         string cond2_code =
-                cond2_name + " = icmp sle i32 " + num.var_name + ", " + to_string(set.type.getSetRange().second);
+                cond2_name + " = icmp sle i32 " + num.var_name + ", " +
+                to_string(set.type.getSetRange().second);
         CodeBuffer::instance().emit(cond2_code);
         int cond2_loc = CodeBuffer::instance().emit("br i1 " + cond2_name + ", label @, label @");
         string true2_label = CodeBuffer::instance().genLabel();
@@ -376,17 +390,18 @@ public:
 
     }
 
-    static string getLocInSet(const Expression &set, const Expression &num) {
+    static string getLocInSet(const Expression& set, const Expression& num){
         std::string loc_in_arr = gimmeANewCuteVar();
         std::string loc_in_mem = gimmeANewCuteVar();
 
         print3ACArithmetic(loc_in_arr, num.var_name, "sub", to_string(set.type.getSetRange().first));
-        std::string get_element = "getelementptr [256 * i1], [256 * i1]* " + set.var_name + " i1 0, i1 " + loc_in_arr;
+        std::string get_element =
+                "getelementptr [256 * i1], [256 * i1]* " + set.var_name + " i1 0, i1 " + loc_in_arr;
         CodeBuffer::instance().emit(loc_in_mem + " = " + get_element);
         return loc_in_mem;
     }
 
-    static std::string byteOverflow(const std::string &val) {
+    static std::string byteOverflow(const std::string& val){
         auto temp_var_1 = gimmeANewCuteVar();
         auto temp_var_2 = gimmeANewCuteVar();
         CodeBuffer::instance().emit(temp_var_1 + " = trunc i32 " + val + " to i8");
@@ -396,25 +411,25 @@ public:
 
 
     inline static void
-    print3ACArithmetic(const std::string &lval, const std::string &left_val, std::string op,
-                       const std::string &right_val) {
+    print3ACArithmetic(const std::string& lval, const std::string& left_val, std::string op,
+                       const std::string& right_val){
         CodeBuffer::instance().emit(lval + " = " + op + " i32 " + left_val + ", " + right_val);
     }
 
-    static Types_enum byteIntCheck(const Expression &t1, const Expression &t2) {
-        if (!t1.isNum() || !t2.isNum()) {
+    static Types_enum byteIntCheck(const Expression& t1, const Expression& t2){
+        if(!t1.isNum() || !t2.isNum()){
             output::errorMismatch(yylineno);
             exit(1);
         }
-        if (t1.type == Types_enum::BYTE_TYPE && t2.type == Types_enum::BYTE_TYPE) {
+        if(t1.type == Types_enum::BYTE_TYPE && t2.type == Types_enum::BYTE_TYPE){
             return Types_enum::BYTE_TYPE;
-        } else {
+        }else{
             return Types_enum::INT_TYPE;
         }
     }
 
-    Expression cast(const Types t) {
-        if (t != Types_enum::INT_TYPE || type.getType() != Types_enum::SET_TYPE) {
+    Expression cast(const Types t){
+        if(t != Types_enum::INT_TYPE || type.getType() != Types_enum::SET_TYPE){
             output::errorMismatch(yylineno);
             exit(1);
         }
@@ -425,17 +440,17 @@ public:
         return new_var;
     }
 
-    static Expression call(const Types &type) { return Expression(type); }
+    static Expression call(const Types& type){ return Expression(type); }
 
-    void typeCheck(const Types t) {
-        if (t != type && !(type == Types_enum::BYTE_TYPE && t == Types_enum::INT_TYPE)) {
+    void typeCheck(const Types t){
+        if(t != type && !(type == Types_enum::BYTE_TYPE && t == Types_enum::INT_TYPE)){
             output::errorMismatch(yylineno);
             exit(1);
         }
     }
 
-    static Expression printNum(int num, Types_enum type) {
-        if (type == Types_enum::BYTE_TYPE && num > 255) {
+    static Expression printNum(int num, Types_enum type){
+        if(type == Types_enum::BYTE_TYPE && num > 255){
             output::errorByteTooLarge(yylineno, std::to_string(num));
             exit(1);
         }
@@ -444,7 +459,7 @@ public:
         return new_var;
     }
 
-    static std::string gimmeANewCuteVar() {
+    static std::string gimmeANewCuteVar(){
         return "%newCuteVar_" + std::to_string(new_var_counter++);
     }
 
