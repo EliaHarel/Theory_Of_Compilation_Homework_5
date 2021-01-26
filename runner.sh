5aeb7c8fcd3d64bc4e6a20e7d3f516af42f79cd8
EliaHarel <EliaHarel@users.noreply.github.com>
2021-01-26 12:14:00 +0200

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,39 @@
+
+function run_all_tests {
+  if [ ! -f $exe_path ]; then
+    echo "Executable file is not found!"
+    exit
+  fi
+  echo "Start."
+  for var in {1..24};
+  do
+    if [ ! -f t${var}.in ]; then
+      continue
+    fi
+    echo -n "running test #$var ... "
+    ./hw5 < t${var}.in 2>&1 > t${var}.ll
+    lli t${var}.ll > t${var}_my.out
+    diff t${var}.out t${var}_my.out &> /dev/null
+    if [[ $? != 0 ]]
+    then
+      echo "Failed!"
+      sleep 1
+      #   vimdiff t${var}.out t${var}_my.out
+      #  exit
+    else echo "ok"
+    fi
+  done
+
+}
+
+# if [ "$#" -ne 1 ]; then
+# echo "Illegal number of parameters. Must be 1."
+# exit
+# fi
+
+make
+cp hw5 hw5tests/
+
+cd hw5tests
+
+run_all_tests hw5
